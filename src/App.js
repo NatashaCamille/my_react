6cfd3b5a257ecbb5a7605f3dbee282fa96a18530
NatashaCamille <nnalubega@outlook.com>
2023-07-04 18:45:59 +0300

    Adds posters

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import SearchIcon from './search.svg'
 
 const API_URL = 'http://www.omdbapi.com?apikey=43ca138d'
 
+const movie1 = {
+        "Title": "Batman Begins",
+        "Year": "2005",
+        "imdbID": "tt0372784",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+}
+
 const App = () => {
 
     const searchMovies = async (title) => {
@@ -19,8 +27,23 @@ const App = () => {
     }, []);
 
     return (
-        <div>
+        <div className="app">
           <h1>MovieHub</h1>  
+
+          <div className="search">
+            <input placeholder="Search for movies" value="Superman" onChange={() => {}}/>
+            <img src={SearchIcon} alt="search" onClick={() => {}} />
+          </div>
+          <div className="container">
+            <div className="movie">
+                <div>
+                    <p>{movie1.Year}</p>
+                </div>
+                <div>
+                    <img src={movie1.Poster} alt={movie1.Title}/>
+                </div>
+            </div>
+          </div>
         </div>
     );
 }
