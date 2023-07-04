25c27ef07004993de81a4ab27c493bd031674599
NatashaCamille <nnalubega@outlook.com>
2023-07-04 19:04:41 +0300

    More edits

--- src/App.js
+++ src/App.js
@@ -40,7 +40,10 @@ const App = () => {
                     <p>{movie1.Year}</p>
                 </div>
                 <div>
-                    <img src={movie1.Poster} alt={movie1.Title}/>
+                    <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                </div>
+                <div>
+                    <span>{movie1.Type}</span>
                 </div>
             </div>
           </div>
